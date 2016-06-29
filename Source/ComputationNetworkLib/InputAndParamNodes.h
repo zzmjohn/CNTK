fc7d9f2bdc45aa665084ad11755598e574e032f0
Clemens Marschner <clemensm@microsoft.com>
2016-06-29 22:58:40 +0200

    Quantized times, first version

--- Source/ComputationNetworkLib/InputAndParamNodes.h
+++ Source/ComputationNetworkLib/InputAndParamNodes.h
@@ -285,6 +285,11 @@ protected:
 public:
     virtual const std::wstring GetRequestedDynamicAxis() const { return m_dynamicAxisNodeName; }
 
+    virtual bool ValueIsConstSelf() const override
+    {
+        return false;
+    }
+
     virtual void Save(File& fstream) const override
     {
         Base::Save(fstream);
