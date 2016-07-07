4cb5138d3533a5c47d8edf760221fcaf1fec4c1d
Clemens Marschner <clemensm@microsoft.com>
2016-07-07 13:31:43 +0200

    Cleanup

--- Source/ComputationNetworkLib/LinearAlgebraNodes.h
+++ Source/ComputationNetworkLib/LinearAlgebraNodes.h
@@ -8,8 +8,6 @@
 #include "ComputationNode.h"
 #include "Matrix.h"
 #include "TensorView.h"
-#include "BlockMultiplier.h"
-
 #include <unordered_set>
 #include <map>
 #include <string>
@@ -709,7 +707,7 @@ public:
     {
         if (m_preparedA != nullptr)
         {
-             BlockMultiplier<BlockHandlerSSE>::FreeMatrix(m_preparedA); // Template arg doesn't matter.
+             TensorView<ElemType>::FreeQuantizedMatrix(m_preparedA); 
         }
     }
 };
