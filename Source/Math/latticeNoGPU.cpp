0ee594370ae7d62c12beb8e5c09f183f4492bdbf
Eldar Akchurin <eldak@microsoft.com>
2016-10-13 17:08:58 +0200

    Fixing release build

--- Source/Math/latticeNoGPU.cpp
+++ Source/Math/latticeNoGPU.cpp
@@ -1,3 +1,5 @@
+#include "stdafx.h"
+
 #ifdef CPUONLY
 #ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS // "secure" CRT not available on all platforms  --add this at the top of all CPP files that give "function or variable may be unsafe" warnings
